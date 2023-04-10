b037b5b66d03de12fe1c747b517acfd8c641b3c3
Tarek Badr <tarekbadr@Tareks-MacBook-Pro.local>
2023-04-10 16:02:21 +0200

    update wornings

--- js/content.js
+++ js/content.js
@@ -1,28 +1,28 @@
-function hide_notification() {
-    var div = document.getElementById("notification-box")
-    // remove the div element after 5 seconds
-    setTimeout(function () {
-        div.parentNode.removeChild(div);
-    }, 2000);
-}
+// function hide_notification() {
+//     var div = document.getElementById("notification-box")
+//     // remove the div element after 5 seconds
+//     setTimeout(function () {
+//         div.parentNode.removeChild(div);
+//     }, 2000);
+// }
 
-function show_notification(request) {
-    var div = document.createElement('div');
-    // create a div element for the HTML
-    div.setAttribute('id', 'notification-box');
-    div.innerHTML = request.greeting;// 'YOUR HTML CODE HERE';
+// function show_notification(request) {
+//     var div = document.createElement('div');
+//     // create a div element for the HTML
+//     div.setAttribute('id', 'notification-box');
+//     div.innerHTML = request.greeting;// 'YOUR HTML CODE HERE';
 
-    // set the CSS properties for the div element
-    div.style.position = 'fixed';
-    div.style.bottom = '0';
-    div.style.right = '0';
-    div.style.zIndex = '9999';
+//     // set the CSS properties for the div element
+//     div.style.position = 'fixed';
+//     div.style.bottom = '0';
+//     div.style.right = '0';
+//     div.style.zIndex = '9999';
 
-    // append the div element to the body of the web page
-    document.body.appendChild(div);
+//     // append the div element to the body of the web page
+//     document.body.appendChild(div);
 
-    hide_notification();
-}
+//     hide_notification();
+// }
 
 browser.runtime.onMessage.addListener((request) => {
     console.log("Message from the background script:");
