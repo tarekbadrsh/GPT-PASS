d3c37cac507f57dff819bb448afad98ecc22c191
Tarek Badr <tarekbadr@Tareks-MacBook-Pro.local>
2023-05-02 21:02:47 +0200

    fix addGptPassButton issue

--- js/content.js
+++ js/content.js
@@ -157,8 +157,7 @@ const handleFacebook = () => {
             continue;
         }
         if (emailInText(span.textContent)) {
-
-            (span);
+            addGptPassButton(span);
         }
     }
 };
